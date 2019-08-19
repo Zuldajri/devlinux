b53d4ecdb9fde2db195770a7937a8adcd51d2366
Zuldajri <w.matyas15@hotmail.fr>
2019-08-19 15:35:00 +0200

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -21,3 +21,9 @@ echo "startxfce4" >> /etc/xrdp/startwm.sh
 sudo service xrdp restart
 
 sudo apt-get install firefox -y
+
+sudo sh -c 'echo "deb [arch=amd64] https://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list'
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo apt-get update -y
+
+sudo apt-get install google-chrome-stable -y
