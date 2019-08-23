a55d8fbd3f8196a33137c08ca49e37e7945c1ba5
Zuldajri <w.matyas15@hotmail.fr>
2019-08-23 20:19:50 +0200

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -67,5 +67,16 @@ sudo apt install virtualbox -y
 #sudo apt install virtualbox-ext-pack
 
 # Install Mini Kube
-
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_1.3.1.deb  && sudo dpkg -i minikube_1.3.1.deb
+
+# Install Azure CLI
+sudo apt-get update -y
+sudo apt-get install ca-certificates curl apt-transport-https lsb-release gnupg -y
+curl -sL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/microsoft.asc.gpg > /dev/null
+AZ_REPO=$(lsb_release -cs)
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | sudo tee /etc/apt/sources.list.d/azure-cli.list
+sudo apt-get update -y
+sudo apt-get install azure-cli
+
+# Install AWS CLI
+sudo apt-get install awscli -y
